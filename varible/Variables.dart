a32ce109ec5b3d00c8a653c86ac9e0d2876eccee
shiran02 <shirankumarasingha97@gmail.com>
2023-06-05 05:15:21 +0530

    first commit

---
+++ varible/Variables.dart
@@ -0,0 +1,6 @@
+main(List<String> arguments){
+  //Variable  = spot inmemory or just a bucket to hold a value
+
+  var country = "Srilanka";  // double or single quotes can fine
+  print(country);
+}
