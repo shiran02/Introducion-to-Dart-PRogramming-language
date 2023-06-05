d06a5fa74785e689822b91903f656f861d9c3599
shiran02 <shirankumarasingha97@gmail.com>
2023-06-05 19:41:22 +0530

    second commit

---
+++ flow-controls/flowcontrols.dart
@@ -0,0 +1,68 @@
+main(List<String> argument){
+
+
+/*
+Notes ...................................
+
+* as,is and is!
+* logical operaters (! , || , &&)
+* OR(||) -> Only Needs one "Side" to be True for the whole expression is 
+  true
+* 
+
+
+*/  
+
+//----------------------------------------------------------------
+//----------------------------------------------------------------
+//----------------------------------------------------------------
+
+
+//for loop ...............................
+
+var fruits = "Mango";
+
+for(var i=0;i<10;i++){
+  if(i%2 == 0){
+    print("Hello Srilanka $fruits");
+  }
+}
+
+
+//logical operaters .......................
+
+
+/*
+String name = "Shiran";
+
+int number = 34;
+int numberTwo = 2;
+
+double pi = 3.12;
+double gravity = 9.6;
+
+print(number is String);      // is
+print(number is int);         // is
+
+print(number is! bool);       //not..
+
+*/
+
+
+
+//if statment .......................
+
+  // if(number != 34){
+  //   print("if true , this will run");
+  // }else{
+  //   print("${name.toUpperCase()}");
+  // }
+
+  // if(!(number != 100) || number >= 100){   //false ot true
+  //   print("Not a hundred");
+  // }else{
+  //   print("Yes");
+  // }
+
+
+}
